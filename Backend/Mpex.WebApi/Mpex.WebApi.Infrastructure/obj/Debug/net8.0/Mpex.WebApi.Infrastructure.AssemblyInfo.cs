52fd39580b88a45c06cf5237600ce6eadf20aea3
LEGION\Martin <martiniliev1311@gmail.com>
2024-12-11 23:58:27 +0200

    Add first .gitignore

--- Backend/Mpex.WebApi/Mpex.WebApi.Infrastructure/obj/Debug/net8.0/Mpex.WebApi.Infrastructure.AssemblyInfo.cs
+++ Backend/Mpex.WebApi/Mpex.WebApi.Infrastructure/obj/Debug/net8.0/Mpex.WebApi.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mpex.WebApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fc4a3aa52ad90327396b6409092066fec22edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mpex.WebApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mpex.WebApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/Mpex.WebApi/Mpex.WebApi/obj/Debug/net8.0/Mpex.WebApi.AssemblyInfo.cs
+++ Backend/Mpex.WebApi/Mpex.WebApi/obj/Debug/net8.0/Mpex.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mpex.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fc4a3aa52ad90327396b6409092066fec22edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mpex.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mpex.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
