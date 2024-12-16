9b46ddd0a5b6b2f647d4c2d33aa9c1fd210544f2
LEGION\Martin <martiniliev1311@gmail.com>
2024-12-16 02:00:16 +0200

    Deleted unnecessary backup file: Backend/Mpex.WebApi/.vs/Mpex.WebApi/v17/DocumentLayout.backup.json

---
+++ Frontend - Web/src/main.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, 
+      cacheTime: 1000 * 60 * 10, 
+      retry: 2, 
+    },
+    mutations: {
+      retry: 2, 
+    },
+  },
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
