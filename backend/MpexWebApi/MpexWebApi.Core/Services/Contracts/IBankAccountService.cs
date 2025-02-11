454ca7825d5205e330cea7c4c3abe20a7afa5aeb
VasilNikolov04 <vasil.nik180@gmail.com>
2025-02-11 20:50:34 +0200

    Created Bank account service and prepared the needed actions

---
+++ backend/MpexWebApi/MpexWebApi.Core/Services/Contracts/IBankAccountService.cs
@@ -0,0 +1,28 @@
+﻿using MpexWebApi.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MpexWebApi.Core.Services.Contracts
+{
+    public interface IBankAccountService
+    {
+        Task<BankAccount> CreateBankAccountAsync(string userId, int accountPlan);
+
+        Task<Card> CreateCardAsync(string bankAccountId);
+
+        Task<bool> FreezeCard(string cardId);
+
+        Task<bool> Deposit(string bankAccountId, string userId, decimal amount);
+
+        Task<bool> Withdraw(string bankAccountId, string userId, decimal amount);
+
+        Task TransferToIBAN(string bankAccountId, string senderId, string receiverIBAN, decimal amount);
+
+        Task TransferBetweenOwnAccounts(string bankAccountId, string senderId, string receiverIBAN, decimal amount);
+        
+        Task<bool> DisableBankAccount(string userId, string bankAccountId);
+    }
+}
