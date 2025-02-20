ab87c584b58907805eea20dfa61f2d0ac4d5bac1
LEGION\Martin <martiniliev1311@gmail.com>
2025-02-20 18:51:08 +0200

    Add ShadCN UI components library

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div></div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
